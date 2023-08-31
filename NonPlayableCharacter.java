cfb644d833094b01d15de1d94680ecc8cfff4421
Wyatt Markham <wymark@colostate.edu>
2023-08-31 17:35:35 -0600

    Fixed formating

--- NonPlayableCharacter.java
+++ NonPlayableCharacter.java
@@ -6,34 +6,33 @@ public class NonPlayableCharacter {
         NonPlayableCharacter npc1 = new NonPlayableCharacter("Fred");
         if(!npc1.playable){
             if(!npc1.resting){
-            switch (npc1.action){
-        case "talking":
-    npc1.resting = false;
-            System.out.println(npc1.name + ": What a marvelous day!");
-                npc1.resting = true;
-            npc1.action = "default";
-        break;
-                case "moving":
-            //insert functionality for moving
-            //this will take several lines
-            //of code
-        case "eating":
-                //insert functionality eating
-                //this will take several lines
-            //of code
-                case "drinking":
+                switch (npc1.action){
+                    case "talking":
+                        npc1.resting = false;
+                        System.out.println(npc1.name + ": What a marvelous day!");
+                        npc1.resting = true;
+                        npc1.action = "default";
+                        break;
+                    case "moving":
+                        //insert functionality for moving
+                        //this will take several lines
+                        //of code
+                    case "eating":
+                        //insert functionality eating
+                        //this will take several lines
+                        //of code
+                    case "drinking":
                         //insert functionality for drinking
                         //this will take several lines
-            //of code
-                break;
-        default:
-                npc1.function1();
-            } //switch
-    
+                        //of code
+                        break;
+                    default:
+                        npc1.function1();
+                } //switch
             } //if
-            } //if
-    //resting state
-        } //end of main
+        } //if
+        //resting state
+    } //end of main
 
     public NonPlayableCharacter(String name){
         this.name = name;
