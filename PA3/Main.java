347ea120403e80c3d157db1e65291027ebf02c57
Wyatt Markham <wymark@colostate.edu>
2023-10-07 19:34:56 -0600

    Forgot to save Main

--- PA3/Main.java
+++ PA3/Main.java
@@ -5,4 +5,15 @@ public class Main {
         Game game = new Game(true,300.0);
         game.gamePlay();
     }
+    public void exposeGameSetup(){
+        System.out.println("Makes difficulty NA for now Then makes store then calls makeItems then makes player");
+    }
+
+    public void exposeGamePlay(){
+        System.out.println("Shows the store inventory then allows the user to interacte with the store menu");
+    }
+
+    public void exposeGameStop(){
+        System.out.println("For now just prints exiting program");
+    }
 }
